000969d1dbe07bc75c5d51e201d4d5b0376b4cff
Gokmen imamoğlu <gokmenim@yahoo.com>
2024-02-12 13:23:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import requests
+import pandas as pd
+from lightweight_charts.widgets import StreamlitChart
 
+if "symbols_list" not in st.session_state:
+    st.session_state.symbols_list = None
+    
+st.set_page_config(
+    layout = 'wide',
+    page_title = 'Crypto Dashboard'
+)
+
+st.markdown(
+    """
+    <style>
+        footer {display: none}
+        [data-testid="stHeader"] {display: none}
+    </style>
+    """, unsafe_allow_html = True
+)
